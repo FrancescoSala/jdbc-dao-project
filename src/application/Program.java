2fe9d3e2818153d80eb379997e54de24b376cea0
Francesco Salamone <fransalamone@gmail.com>
2025-01-29 17:37:46 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,6 +18,7 @@ public class Program {
         Seller seller = sellerDAO.findById(3);
         System.out.println(seller);
 
+
         System.out.println("\n=== TEST 2: seller findByDepartment ===");
         Department department = new Department(2, null);
         List<Seller> list = sellerDAO.findByDepartment(department);
@@ -26,6 +27,7 @@ public class Program {
             System.out.println(obj);
         }
 
+
         System.out.println("\n=== TEST 3: seller findAll ===");
         list = sellerDAO.findAll();
 
@@ -33,10 +35,18 @@ public class Program {
             System.out.println(obj);
         }
 
+
         System.out.println("\n=== TEST 4: seller insert ===");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDAO.insert(newSeller);
         System.out.println("Inserted! New id: " + newSeller.getId());
+
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDAO.findById(1);
+        seller.setName("Martha Waine");
+        sellerDAO.update(seller);
+        System.out.println("Update completed!");
 
     }
 }
